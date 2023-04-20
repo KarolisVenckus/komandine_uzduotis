941383f5cead2faf5fde58ad89a7d4374504e2b6
JurateKru <jurate.krupaviciene@gmail.com>
2023-04-20 16:16:27 +0300

    pridedu balanso likucio klase

--- biudzetas.py
+++ biudzetas.py
@@ -8,3 +8,20 @@ class Vartotojas:
     def __get_biudzetas(self):
         print(f'Jūsų dabartinsi biudžetas: {self.__biudzetas}')
     
+
+
+class BalansoLikutis:
+    def __init__(self, balansas, pin_kodas):
+        self.__balansas = balansas
+        self.__pin_kodas = pin_kodas
+
+    def rodyti_balansa(self, suma, pin_kodas):
+        if pin_kodas == self.__pin_kodas:
+            self.__balansas -= suma
+            print(f'PIN kodas teisingas. Sąskaitos likutis - {self.__balansas} EUR')
+        else:
+            print(f'PIN kodas neteisingas')
+
+saskaita = BalansoLikutis(1000, 0000)
+
+saskaita.rodyti_balansa(100, int(input("Įveskite 4 skaitmenų PIN kodą:")))
